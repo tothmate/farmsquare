05cc5f18a28323e6b3653525de6f869685ab867d
Akos Toth-Mate <akos@tothmate.com>
2011-01-01 22:43:26 +0100

    foursquare oauth redirect

--- app/apps/foursquare_connect/handlers.py
+++ app/apps/foursquare_connect/handlers.py
@@ -3,7 +3,23 @@ from tipfy.ext.jinja2 import render_response
 from tipfy.ext.session import SessionMiddleware, SessionMixin
 
 import foursquare
-import oauth
+
+
+class VenueListHandler(RequestHandler, SessionMixin):
+    middleware = [SessionMiddleware]
+
+    def get(self):
+        # TODO: move it to a decorator
+        if self.session["fs"] is None:
+            return self.redirect("/foursquare-login")
+        fs = self.session["fs"]
+        
+        try:
+            venues = fs.user()
+        except foursquare.FoursquareException:
+            return self.redirect("/foursquare-login")
+        
+        return render_response("foursquare_login_successful.html", message=venues)
 
 
 class LoginHandler(RequestHandler, SessionMixin):
@@ -14,14 +30,14 @@ class LoginHandler(RequestHandler, SessionMixin):
         credentials = foursquare.OAuthCredentials("I2Z50NWD45L0XCZNARRS3A15TTKCKSI0WWR2A3HCYHZDIE44", "Z2PBQ40CDNBVRNNSM4JK5ZVPK0IWWEVJKTBMWOGM3PIXOA4Y")
         fs = foursquare.Foursquare(credentials)
         # TODO: don't hardcode server
-        app_token = fs.request_token(oauth_callback="http://localhost:8080/foursquare-login-success")
+        app_token = fs.request_token(oauth_callback="http://localhost:8080/foursquare-login-success-callback")
         auth_url = fs.authorize(app_token)
         self.session["fs"] = fs
         self.session["app_token"] = app_token
         return render_response("foursquare_login.html", message=auth_url)
 
 
-class LoginSuccessHandler(RequestHandler, SessionMixin):
+class LoginSuccessCallbackHandler(RequestHandler, SessionMixin):
     middleware = [SessionMiddleware]
 
     def get(self):
@@ -29,10 +45,12 @@ class LoginSuccessHandler(RequestHandler, SessionMixin):
         
         # TODO: error handling
         oauth_verifier = self.request.args.get("oauth_verifier")
-        # NOTE: Avoid storing it session?
+        # NOTE: Avoid storing it in session?
         fs = self.session["fs"]
         app_token = self.session["app_token"]
         
         user_token = fs.access_token(app_token, oauth_verifier)
         fs.credentials.set_access_token(user_token)
-        return render_response("foursquare_login_successful.html", message=fs.user())
+        del self.session["app_token"]
+        
+        return self.redirect("/")
--- app/apps/foursquare_connect/urls.py
+++ app/apps/foursquare_connect/urls.py
@@ -3,8 +3,9 @@ from tipfy import Rule
 
 def get_rules(app):
     rules = [
+        Rule('/', handler='apps.foursquare_connect.handlers.VenueListHandler'),
         Rule('/foursquare-login', handler='apps.foursquare_connect.handlers.LoginHandler'),
-        Rule('/foursquare-login-success', handler='apps.foursquare_connect.handlers.LoginSuccessHandler'),
+        Rule('/foursquare-login-success-callback', handler='apps.foursquare_connect.handlers.LoginSuccessCallbackHandler'),
     ]
 
     return rules
